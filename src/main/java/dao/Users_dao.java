c4e7f288668cea528491133e9c9bf0ae6936cc84
mohammadsohrab <itsjustsohrab@gmail.com>
2025-02-05 23:09:33 +0530

    .

--- src/main/java/dao/Users_dao.java
+++ src/main/java/dao/Users_dao.java
@@ -1,15 +1,9 @@
 package dao;
 
-
-
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-
-
-
 
 import dto.Users;
 
